657b7571d49c1c07ec4c3333490360ef63d86eaa
Hadeed Ul Hassan <hadeed.hassan189@gmail.com>
2026-02-10 23:06:37 +0500

    Set up the pipelines

--- routes/subcription.router.js
+++ routes/subcription.router.js
@@ -4,4 +4,18 @@ const subscriptionRouter = Router()
 
 subscriptionRouter.get("/", (req,res)=> res.send({ message : "Get all subcriptions"}))
 
+subscriptionRouter.get("/:id", (req,res)=> res.send({ message : "Get subcriptions details"}))
+
+subscriptionRouter.post("/", (req,res)=> res.send({ message : "Create  subcriptions"}))
+
+subscriptionRouter.put("/:id", (req,res)=> res.send({ message : "Updtate the subcriptions"}))
+
+subscriptionRouter.delete("/:id", (req,res)=> res.send({ message : "Delete all subcriptions"}))
+
+subscriptionRouter.get("/users/:id", (req,res)=> res.send({ message : "Get all users subcriptions"}))
+
+subscriptionRouter.put("/:id/cancel", (req,res)=> res.send({ message : "Camcel the subcriptions"}))
+
+subscriptionRouter.get("/upcoming-renewals", (req,res)=> res.send({ message : "Get Upcoming Renewals"}))
+
 export default subscriptionRouter
---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("Test completed successfully!");
+
+
+setTimeout(()=> {
+    console.log("Test completed successfully after 3 seconds!");
+}, 3000)
+
+
+console.log("Test Completed successfully immediately!") ;
+
