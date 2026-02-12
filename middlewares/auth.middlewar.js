82889beac9b887d21a303e30eed4d7695a4d1086
Hadeed Ul Hassan <hadeed.hassan189@gmail.com>
2026-02-12 15:13:28 +0500

    Set up the arcjet configuration to protect apis

--- middlewares/auth.middlewar.js
+++ middlewares/auth.middlewar.js
@@ -7,8 +7,8 @@ const authorize = async(req, res, next) => {
 
     try{
             let token;
-            if(req.header.authorization && req.header.authorization.startWith('Bearer ')){
-                token= req.header.authorization.split(' ')[1];
+            if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+                token= req.headers.authorization.split(' ')[1];
 
                 if(!token) return res.status(401).json({ message: 'Unauthorized' });
                 const decoded = jwt.verify(token, JWT_SECRET);
